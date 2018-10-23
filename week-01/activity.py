cc3ecc34641a6963d9bf7645bbff8804ac2297db
Riley Guy <rileygriffinguy@gmail.com>
2018-10-22 23:11:05 -0400

    Created activity.py

---
+++ week-01/activity.py
@@ -0,0 +1,12 @@
+# Try to make a script that makes a madlib.
+# First, ask the user for things like adjectives and nouns.
+# Then, put them in the format of a story.
+
+# For example, you might first ask the user to input an adjective and a noun
+# and then insert them into a story or sentence you've already written.
+
+# The last part of the script might go something like this:
+print("I was walking down the street whenever I saw a " + adjective1 + ' ' + noun1 +
+      " come out of nowhere!")
+
+# Make it as long as you want.
