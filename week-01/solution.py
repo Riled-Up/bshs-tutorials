c8aafbb8cc5aa3a18c5d058ac482a3957fc34598
Riley Guy <rileygriffinguy@gmail.com>
2018-10-22 23:27:16 -0400

    Created solution.py

---
+++ week-01/solution.py
@@ -0,0 +1,14 @@
+# Remember that there are multiple ways of doing things,
+# and my way might not be the best way.
+
+# First, I ask users for their input and store them in the appropriate
+# variables.
+print("Type the name of a person:")
+person = input()
+print("Type an adjective ending in -est:")
+adjective = input()
+print("Type a noun:")
+noun = input()
+
+# Now I print out my story using the variable I just got.
+print(person + " has the " + adjective + ' ' + noun + " I've ever seen.")
